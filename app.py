e458ebb975b35b0840e69573f025c06ca1e34ee7
GoldAndriuks <eh2302is387@ueh.edu.mx>
2024-12-05 08:32:44 -0600

    Paso de variable al html

--- app.py
+++ app.py
@@ -5,4 +5,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    nombre = "Andree Rodriguez"
+    return render_template('index.html', nombre=nombre)
